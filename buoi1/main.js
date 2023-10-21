97027799827545ffbc09ca9368cb9793493dd9a5
hoanghieutt <hhieu5764@gmail.com>
2023-10-21 09:41:40 +0700

    init project

---
+++ buoi1/main.js
@@ -0,0 +1,20 @@
+// console.log("abs");
+// let text1 ="hello";
+// let text2 ="lolo";
+// let text3 = text1.concat(" ",text2);
+
+// document.write(text3);
+
+// bai1
+
+let highest = Math.max(2, 56, 12, 1, 233, 4);
+console.log(highest);
+
+
+let highestOfWords = Math.max("hi", 3, "bye");
+console.log(highestOfWords);
+
+console.log(1)
+
+alert('hello would hỏi dân lập trình 1')
+
